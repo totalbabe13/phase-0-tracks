44657ec94919475ba489821858e75b35e169c98e
totalbabe13 <kaplanleonardo@gmail.com>
2017-06-26 17:56:13 -0500

    modifying responses to have boolean values, creating comparisons, set new variables for vampires vs humans

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -26,7 +26,7 @@ puts
 puts
 
 puts 'Question Number 2: How old are you? What year were you born? 
-(please answer with your birth year ) ' #numeric/integer value
+(please answer with your birth year ) ' #numeric/integer value converted to boolean
 age_of_person = gets.chomp
 
 	if age_of_person.to_i >= 1960 #true?
@@ -39,13 +39,20 @@ age_of_person = gets.chomp
 	   	   
 puts 
 puts 'Oh I see...Let me just write that one down.'
-
 puts
 puts
 
 puts 'Question Number 3: Our company cafeteria serves garlic bread. Should we order some for you? (y/n) '
-garlic_allergy = gets.chomp #yes or no value 
+garlic_allergy = gets.chomp #yes or no value convert to boolean
 puts 'ok you got it.'
+
+	if garlic_allergy = y
+		garlic_allergy = true
+
+	else garlic_allergy = n
+		garlic_allergy = false
+		
+	end
 
 puts
 puts
@@ -54,11 +61,28 @@ puts 'Question Number 4: Would you like to enroll in the company’s health insu
 health_insurance_prefrence = gets.chomp
 puts 'great!, ok thank you for you\'re time!' #yes or no value
 
+	if health_insurance_prefrence = y
+		health_insurance_prefrence = true
 
-age_of_person_vampire = false
+	else health_insurance_prefrence = n
+		  health_insurance_prefrence = false
+	end	  	
 
-puts age_of_person && age_of_person_vampire 
-puts age_of_person || age_of_person_vampire 
+#VAMPIRE VALUES
+age_of_person_vampire              = false
+garlic_allergy_vampire             = false
+health_insurance_prefrence_vampire = false
+
+#HUMAN values
+age_of_person              = true
+garlic_allergy             = true
+health_insurance_prefrence = true
+
+
+
+
+#puts age_of_person && age_of_person_vampire 
+#puts age_of_person || age_of_person_vampire 
 
 
 
