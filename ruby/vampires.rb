732b41d95ae325c291c0dd1f449eb002c1ccfe1e
totalbabe13 <kaplanleonardo@gmail.com>
2017-06-26 22:09:43 -0500

    adding employee loop to count how many employees are being processed. working on how to return vampire values

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,6 +4,19 @@
 #Should we order some for you? 
 #Interview With A Vampire
 
+#At the beginning of your program, ask the user how many employees will be processed, then begin the survey process for the first employee. After you print the result for that employee, start the survey over again, and repeat until all the employees have been processed.
+
+#(Hmm, doing things over and over again ... that certainly sounds like a loop, doesn't it?)
+
+puts 'Hello, how many employees will you be processing today?'
+amount_of_employees = gets.chomp 
+"Lets get started"
+
+
+while amount_of_employees.to_i != 0 
+
+
+
 puts 
 puts
 puts 'Welcome to your interview,'
@@ -26,13 +39,13 @@ puts
 puts
 
 puts 'Question Number 2: How old are you? What year were you born? 
-(please answer with your birth year ) ' #numeric/integer value converted to boolean
-age_of_person = gets.chomp
+      (please answer with your birth year ) ' #numeric/integer value converted to boolean
+      age_of_person = gets.chomp
 
-	if age_of_person.to_i >= 1960 #true?
+	if age_of_person.to_i >= 1900 #true?
 	   age_of_person = true
 
-	else age_of_person.to_i <= 1960
+	else age_of_person.to_i <= 1900
 		  age_of_person = false
 
 	end		  
@@ -42,47 +55,88 @@ puts 'Oh I see...Let me just write that one down.'
 puts
 puts
 
-puts 'Question Number 3: Our company cafeteria serves garlic bread. Should we order some for you? (y/n) '
-garlic_allergy = gets.chomp #yes or no value convert to boolean
+puts 'Question Number 3: Our company cafeteria serves garlic bread.      Should we order some for you? (y/n) '
+      garlic_allergy = gets.chomp #yes or no value convert to boolean
 puts 'ok you got it.'
 
-	if garlic_allergy = y
-		garlic_allergy = true
+	if   garlic_allergy = 'y'
+		   garlic_allergy = true
 
-	else garlic_allergy = n
-		garlic_allergy = false
+	else garlic_allergy = 'n'
+		   garlic_allergy = false
 		
 	end
 
 puts
 puts
-
-puts 'Question Number 4: Would you like to enroll in the company’s health insurance? (it\'s got FULL dental...y/n)'
-health_insurance_prefrence = gets.chomp
+puts 'Question Number 4: Would you like to enroll in the company’s       health insurance? (it\'s got FULL dental...y/n)'
+      health_insurance_prefrence = gets.chomp
 puts 'great!, ok thank you for you\'re time!' #yes or no value
+puts 
+puts
+puts
+	if   health_insurance_prefrence = 'y'
+		   health_insurance_prefrence = true
 
-	if health_insurance_prefrence = y
-		health_insurance_prefrence = true
-
-	else health_insurance_prefrence = n
-		  health_insurance_prefrence = false
+	else health_insurance_prefrence = 'n'
+		   health_insurance_prefrence = false
 	end	  	
 
-#VAMPIRE VALUES
-age_of_person_vampire              = false
-garlic_allergy_vampire             = false
-health_insurance_prefrence_vampire = false
-
-#HUMAN values
-age_of_person              = true
-garlic_allergy             = true
-health_insurance_prefrence = true
 
 
 
+#WE NEED FOUR OUTPUTS: 
 
-#puts age_of_person && age_of_person_vampire 
-#puts age_of_person || age_of_person_vampire 
+  #1 - This person is a VAMPIRE 
+  #2 - This person is a HUMAN
+  #3 - this person is PROBABLY a VAMPIRE
+  #4 - INCONCLUSIVE
+
+
+
+  if (age_of_person && garlic_allergy) && health_insurance_prefrence == true
+     puts 'This is a human being, not a vampire.'
+  
+ 
+  elsif (age_of_person && garlic_allergy) && health_insurance_prefrence == false
+     puts ' This is vampire, not a human being. '
+  end 
+  
+  if age_of_person && (garlic_allergy || health_insurance_prefrence) == true
+     puts ' This person is probably a human being.'
+  end
+    
+  if  age_of_person && (garlic_allergy || health_insurance_prefrence) == false
+     puts ' Results inconclusive...'
+  end
+   
+  
+  amount_of_employees = (amount_of_employees.to_i-1)
+end #end employee loop 
+  
+puts
+puts
+
+
+
+
+#If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+
+  #if (age_of_person && garlic bread) || health_insurance_prefrence =
+   #false
+   #puts 'This is a vampire, not a human being'
+  #end
+  
+#If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+  ###end  
+  
+puts
+puts
+
+
+
+
+
 
 
 
