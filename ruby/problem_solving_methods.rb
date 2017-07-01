b757282d93fd882491b6ade59c8216007d13c441
totalbabe13 <kaplanleonardo@gmail.com>
2017-06-30 22:12:20 -0500

    resolved fibonnaci sequence in ruby with partner Nicole G

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,6 +1,6 @@
 array_1 = [24,7,0,420,666,7,11,69]
 
-
+#release 0
 def special_method(array_1,item)
 i = 0
 while array_1[i] != item
@@ -14,3 +14,28 @@ i += 1
   end
  
  special_method(array_1,420)
+
+ # - - - - - - - - - - - - - - - - - - 
+ # Release 1 (fibonnaci exercise)
+
+ def fibonacci(fib)
+value1 = 0 
+value2 = 1
+fib.times do 
+  temp = value1
+  value1 = value2
+  value2 = temp + value2
+end
+  return value1
+end
+  
+array_1 =[]
+5.times do |fib|
+  call_method = fibonacci(fib)
+  #puts call_method
+  puts 
+  array_1.push call_method
+  #puts array_1
+end
+
+puts array_1
