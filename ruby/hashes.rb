045f8a017f647a41a792e42d425db440a54fc1d4
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-04 03:49:10 -0500

    creating interface for user to select input and modify responses

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -49,6 +49,50 @@ puts
 puts "The number of bedrooms your home has : #{client_assesment[:client_bedrooms]}"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+puts 'Would you like to edit or change any of your answers?'
+puts 'If so, select the NUMBER corresponding to your answer, and re-enter your response.'
+puts 'Or, simply press ENTER, to confirm your application.'
+puts
+puts 'Would you like to make any changes?' 
+puts 'Enter a number or press ENTER:'
+       response_edit = gets.to_i
+       if response_edit == 1
+         puts 'Please re-enter your name:'
+         new_name = gets.chomp
+         client_assesment[:client_name] = new_name
+         puts 
+         #puts client_assesment[:client_name]
+      elsif response_edit == 2
+         puts 'Please re-enter your age:'
+         new_age = gets.to_i
+         client_assesment[:client_name] = new_age
+         puts 
+         #puts client_assesment[:client_age] 
+      
+      elsif response_edit == 3
+         puts 'Please re-enter how many children you have:'
+         new_child = gets.to_i
+         client_assesment[:client_children] = new_child
+         puts 
+         #puts client_assesment[:client_children] 
+      
+      elsif response_edit == 4
+         puts 'Please re-enter your theme choice:'
+         new_theme = gets.to_i
+         client_assesment[:client_decor_theme] = new_theme
+         puts 
+         #puts client_assesment[:client_decor_theme] 
+         
+      elsif response_edit == 5
+         puts 'Please re-enter your number of bedrooms:'
+         new_bedrooms = gets.to_i
+         client_assesment[:client_bedrooms] = new_bedrooms
+         puts 
+         #puts client_assesment[:client_bedrooms]    
+         
+       end 
+
+
 #INTRO section / Hello, with brief explanation of use of program
 
 #first hash will ask : 
