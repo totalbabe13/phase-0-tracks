bed80e1e92eebb64a0a50a20d13125464386bb41
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-04 00:36:31 -0500

    speudocoding in program structure

---
+++ ruby/hashes.rb
@@ -0,0 +1,30 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#INTRO section / Hello, with brief explanation of use of program
+
+#first hash will ask : 
+
+#1.NAME - string
+
+#2.AGE - integer 
+
+#3.NUMBER OF CHILDERN -integer
+
+#4.DECORE THEME - string
+
+#5.HOW MANY BEDROOMS- integer
+
+#6.IF THIS IS THIER FIRST HOME -boolean
+
+#7.NAME OF SPOUSE -string
+
+#( use combination of intergers, strings, booleans values.)
+#  - - - - - - - - - - - - - - - - - 
+
+#UPDATES/CORRECTIONS
+
+#ARE YOU SURE OF THE information is correct?
+
+#user would enter key name, and correction 
+#key value would re-re assign 
+#print updated hash
+#exit program
