217bd12d71efedf1c057d66e72fbc7e472ca8f2e
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-04 02:28:00 -0500

    creating hash, and keys. developing interface with user.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,5 +1,42 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 #INTRO section / Hello, with brief explanation of use of program
+puts "Welcome to Iris Interiors INC. We will see to it that what you see is what you get!"
+puts
+puts"Please, complete the following questionaire as accurately as possible, to help us, HELP YOU."
+puts
+puts"Press ENTER to continue:"
+
+client_assesment = {
+            
+}
+puts 'Please, enter your full name:'
+client_assesment[:client_name] = gets.chomp
+#p client_assesment[:client_name]
+puts
+
+puts 'What is your current age in years?'
+client_assesment[:client_age] = gets.to_i
+puts 
+#p client_assesment[:client_age]
+
+puts'How many children do you have?'
+client_assesment[:client_children] = gets.to_i
+puts
+#p client_assesment[:client_children]
+
+puts 'Please tell us what decor theme you had in mind?'
+client_assesment[:client_decor_theme] = gets.chomp
+puts
+#p client_assesment[:client_decor_theme]  
+
+puts 'How many bedrooms does you home include?'
+client_assesment[:client_bedrooms] = gets.to_i
+puts
+#p client_assesment[:client_bedrooms] 
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#INTRO section / Hello, with brief explanation of use of program
 
 #first hash will ask : 
 
