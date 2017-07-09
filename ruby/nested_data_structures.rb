48eff2c3aab04c71d6501510646a1e5d5f797383
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-08 22:52:14 -0500

    finalized presentation of code. adding question that use deep nested data. submitting assignment

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,34 +1,144 @@
 
 
 #BUILDING A GALLERY IN CHICAGO:
-
-
-boyfriends_gallery = {
+boyfriends_exhibitions = {
 	
 	gallery: works = { 
 		
-		 painting1: ['Chicago Sunset Abstract', 'Oil on Canvas', '38 x 46', '2017', '$11,000'],
+	  painting1: ['Chicago Sunset Abstract', 
+		             'Oil on Canvas', 
+		             '38 x 46', '2017', 
+		             '$11,000'],
 	  
-	   painting2:['Birds Fly Over 290','Oil on Canvas', '30 x 30', '2017', '$9,000' ],
+	  painting2: ['Birds Fly Over 290',
+	               'Oil on Canvas', 
+	               '30 x 30', '2017', 
+	               '$9,000' ],
 	  
-	  painting3: ['Lake Michigan Thawing','Oil on Canvas', '60 x 45', '2017', '$12,000' ],
+	  painting3:  ['Lake Michigan Thawing',
+	               'Oil on Canvas', 
+	               '60 x 45', 
+	               '2017', 
+	               '$12,000' ],
 	 
-	  drawing1:  ['Sears Tower','Charcoal on newsprint','38 x 38','2016','$1,000'],
+	  drawing1:  ['Sears Tower',
+	               'Charcoal on newsprint',
+	               '38 x 38',
+	               '2016',
+	               '$1,000'],
 	 
-	  drawing2: ['Study of Garfield Park Conservatory','Charcoal on newsprint', '38 x 38', '2016', '$1,000' ],
+	  drawing2:   ['Study of Garfield Park Conservatory',
+	               'Charcoal on newsprint', 
+	               '38 x 38', 
+	               '2016', 
+	               '$1,000' ],
 	 
-	  drawing3: ['Portrait of Mother at Field Museum','Graphite on Arches','38 x 38','2017','$3,000' ],
+	  drawing3:   ['Portrait of Mother at Field Museum',
+	               'Graphite on Arches',
+	               '38 x 38',
+	               '2017',
+	               '$3,000' ],
 	 
-	  sculpture1: ['Skyline','Casted Bronze','Dimensions variable','2016','$13,000'],
+	  sculpture1: ['Skyline',
+	               'Casted Bronze',
+	               'Dimensions variable',
+	               '2016',
+	               '$13,000'],
 	 
-	  sculpture1: ['The Fresh Coast','Casted Bronze','Dimensions variable','2017','$7,000']
-      },# closing hash gallery
+	  sculpture2: ['The Fresh Coast',
+	               'Casted Bronze',
+	               'Dimensions variable',
+	               '2017',
+	               '$7,000']
+      },
 	
-	office: furniture = ['Desk', 'Computer','Flatfile', 'Chair'],
-      
-	#closing  office
+	
+  office: furniture = { 
+	           
+	           desk:     ['rolodex', 'pens', 'pencils', 'calander'],
+	          
+	           computer:  'I Mac',
+	           
+	           chair:     'black with wheels',
+	           
+	           flatfile: ['works on paper', 
+	                      'Press release', 
+	                      'Posters from exhibitions']
+	},
+  #(key of boyfriends_gallery HASH)    
+	
 
 	storage_racks: ['Paintings', 'Drawings', 'Photographs']
+   #(key of boyfriends_gallery HASH)
 
-#	}#closing hash storage_racks
 }#closing hash boyfriends_gallery
+
+
+
+
+puts
+puts 'Someone is asking for the TITLE of painting2:'
+puts 
+p boyfriends_exhibitions[:gallery][:painting2][0]
+puts ' -  -  -  -  -  -  -  -  -  -  -'
+puts
+
+puts
+puts 'Someone is asking for the MATERIALS of painting1:'
+puts
+p boyfriends_exhibitions[:gallery][:painting1][1]
+puts ' -  -  -  -  -  -  -  -  -  -  -'
+puts
+
+puts
+puts 'Someone is asking for the DIMENSIONS of sculpture2:'
+puts
+p boyfriends_exhibitions[:gallery][:sculpture2][2]
+puts ' -  -  -  -  -  -  -  -  -  -  -'
+puts
+
+
+puts
+puts 'Someone is asking for the YEAR of drawing3:'
+puts
+p boyfriends_exhibitions[:gallery][:drawing3][3]
+puts ' -  -  -  -  -  -  -  -  -  -  -'
+puts
+
+
+puts
+puts 'Someone is asking for the PRICE of sculpture1:'
+puts
+p boyfriends_exhibitions[:gallery][:sculpture1][4]
+puts ' -  -  -  -  -  -  -  -  -  -  -'
+puts
+
+
+puts
+puts 'The new intern is asking whats in the office:'
+puts
+p boyfriends_exhibitions[:office]
+puts ' -  -  -  -  -  -  -  -  -  -  -'
+puts
+
+
+puts
+puts 'The new intern is asking whats in the flatfile:'
+puts
+p boyfriends_exhibitions[:office][:flatfile]
+puts ' -  -  -  -  -  -  -  -  -  -  -'
+puts
+
+puts
+puts 'The new intern is asking where are the photographs stored:'
+puts
+p boyfriends_exhibitions[:storage_racks][2]
+puts ' -  -  -  -  -  -  -  -  -  -  -'
+puts
+
+
+#p boyfriends_gallery[:gallery]
+#puts
+#p boyfriends_gallery[:office]
+#puts
+#p boyfriends_gallery[:storage_racks]
