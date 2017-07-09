191b929e54edf97295a4a388bc03995de58ec660
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-08 21:03:04 -0500

    creating three spaces, GALLERY, OFFICE, STORAGE RACKS. creating objects in side, with details of art works.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,24 +2,33 @@
 
 #BUILDING A GALLERY IN CHICAGO:
 
-boyfriends_gallery {
-	gallery: {
+
+boyfriends_gallery = {
+	
+	gallery: works = { 
 		
+		 painting1: ['Chicago Sunset Abstract', 'Oil on Canvas', '38 x 46', '2017', '$11,000'],
+	  
+	   painting2:['Birds Fly Over 290','Oil on Canvas', '30 x 30', '2017', '$9,000' ],
+	  
+	  painting3: ['Lake Michigan Thawing','Oil on Canvas', '60 x 45', '2017', '$12,000' ],
+	 
+	  drawing1:  ['Sears Tower','Charcoal on newsprint','38 x 38','2016','$1,000'],
+	 
+	  drawing2: ['Study of Garfield Park Conservatory','Charcoal on newsprint', '38 x 38', '2016', '$1,000' ],
+	 
+	  drawing3: ['Portrait of Mother at Field Museum','Graphite on Arches','38 x 38','2017','$3,000' ],
+	 
+	  sculpture1: ['Skyline','Casted Bronze','Dimensions variable','2016','$13,000'],
+	 
+	  sculpture1: ['The Fresh Coast','Casted Bronze','Dimensions variable','2017','$7,000']
+      },# closing hash gallery
+	
+	office: furniture = ['Desk', 'Computer','Flatfile', 'Chair'],
+      
+	#closing  office
 
-	}
+	storage_racks: ['Paintings', 'Drawings', 'Photographs']
 
-
-
-
-
-
-	office: {
-
-	}
-
-	storage_racks: {
-
-	}
-
-
-}
+#	}#closing hash storage_racks
+}#closing hash boyfriends_gallery
