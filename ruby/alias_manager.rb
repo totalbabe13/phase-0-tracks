f0fc7f8fde6281c7f6d8033b8d9c80ea4263f74c
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-10 01:03:16 -0500

    first process, ask if the letter in question is vowel or not

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,27 +1,28 @@
 def agent_name
+  agent = 'Leonardo' #Miupesfu
   
-  agent = 'Leonardo' 
   
-  number_of_letters = agent.length 
+  not_vowels = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+  vowels      = ['a','e','i','o','u']
   
-  code_array1 = agent.downcase.split('')
+   
+  number_of_letters = agent.length  # 8
+  
+  
+  code_array1 = agent.downcase.split('') 
+  #transform string into array, separate characters 
   
   new_code_name = []
   
+  #p code_array1  # ["l", "e", "o", "n", "a", "r", "d", "o"]
   
-  if code_array1[0] != 'a'||'e'||'i'||'o'||'u'
-    first_letter = code_array1.shift
-    puts code_array1
-    puts
-    puts first_letter
-    puts
-    new_letter = first_letter.next
-    puts new_letter
-    puts
-    new_code_name.push new_letter
-    puts new_code_name
-    
-    
+  first_letter = code_array1.first
+  #point to the first letter => 'l'
+  
+  #task 1: ask if letter is vowel or not ?
+  if vowels.any? {|letter| letter == first_letter} == false
+  
   end  
+   
 end
 agent_name
