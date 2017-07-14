af9e50a32bb10d79abf0942735bb1133c5e7e96e
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-14 06:25:38 -0500

    drafting in psuedo code, assignment layout

---
+++ ruby/puppy.rb
@@ -0,0 +1,30 @@
+
+In phase-0-tracks/ruby/puppy_methods.rb:
+
+Add driver code at the bottom that initializes an instance of Puppy, and verify that your instance can now fetch a ball. Run the file from the command line to check your work.
+Add a speak method that takes an integer, and then prints "Woof!" that many times.
+Add a roll_over method that just prints "*rolls over*".
+Add a dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
+Add one more trick -- whichever one you'd like.
+If you haven't already, update your driver code to demonstrate that all of these methods work as expected.
+
+
+#Class ---------------------------
+#Puppy
+
+#CHARACTERISTICS ----------------
+#Eyelash length: LONG
+#cuteness: too much
+#Eye count: 2
+#Legs: 4
+#Tail: 1
+#Color: brown, white with black spots
+
+#BEHAVIOR -----------------------
+#Barks
+#Licks
+#Smells
+#Growls
+#Rolls Over
+#Fetches
+#Wags tail
