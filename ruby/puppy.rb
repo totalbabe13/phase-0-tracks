6fc0b0a3e78a724b5852905cdacde57c2fc48e85
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-14 06:43:32 -0500

    adding speak method to class puppy

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,30 +1,29 @@
 
-In phase-0-tracks/ruby/puppy_methods.rb:
+#In phase-0-tracks/ruby/puppy_methods.rb:
 
-Add driver code at the bottom that initializes an instance of Puppy, and verify that your instance can now fetch a ball. Run the file from the command line to check your work.
-Add a speak method that takes an integer, and then prints "Woof!" that many times.
-Add a roll_over method that just prints "*rolls over*".
-Add a dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
-Add one more trick -- whichever one you'd like.
-If you haven't already, update your driver code to demonstrate that all of these methods work as expected.
+#Add driver code at the bottom that initializes an instance of Puppy, and verify that your instance can now fetch a ball. Run the file from the command line to check your work.
+#Add a speak method that takes an integer, and then prints "Woof!" that many times.
+#Add a roll_over method that just prints "*rolls over*".
+#Add a dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
+#Add one more trick -- whichever one you'd like.
+#If you haven't already, update your driver code to demonstrate that all of these methods work as expected.
 
+class Puppy
 
-#Class ---------------------------
-#Puppy
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
 
-#CHARACTERISTICS ----------------
-#Eyelash length: LONG
-#cuteness: too much
-#Eye count: 2
-#Legs: 4
-#Tail: 1
-#Color: brown, white with black spots
+  def speak(barks)
+    puts ("WOOF!" * 3)
+    barks
+  end
 
-#BEHAVIOR -----------------------
-#Barks
-#Licks
-#Smells
-#Growls
-#Rolls Over
-#Fetches
-#Wags tail
+end
+
+lassy= Puppy.new
+
+lassy.fetch "ball"
+lassy.speak 3
+
