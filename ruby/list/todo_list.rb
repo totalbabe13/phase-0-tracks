016a1eb1d3abec516faa83be23e3a89fea6cf14a
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-17 03:36:15 -0500

    passed first RSPEC test

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,16 @@
+
+
+class List
+	
+
+	def get_items(task1,task2)  
+       todo = []
+
+       todo.push(task1,task2)
+       p todo
+    end
+    #- - - - - - - - - - - - - - - - - - - - - 
+
+ end   
+
+ 
