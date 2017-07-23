ec358c1bcefda2c4d2e15e5c48825dbc92237d05
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-23 18:09:34 -0500

    spuedo coding basic question for completeing module 7.1

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+
+// 7.1 Leonardo Kaplan
+
+// obtain a string 
+
+//can you point to individual characters in string?
+
+// are there built in Java functions? (like ruby?)
+
+//how to put data into an array and manipulate order?
+
+// source data from array--> convert to string (remember that you can add string in JS?)
+
+//RETURN a reverse string
