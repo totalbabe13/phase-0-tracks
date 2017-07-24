2f8bce1d20e1b426c2fc3e740e175f2199f4a518
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-23 22:16:00 -0500

    learning how to use .length

--- js/explore.js
+++ js/explore.js
@@ -2,29 +2,15 @@
 // 7.1 Leonardo Kaplan
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
+function reverser(word){
+return word.length
+}
+
+reverser('apples')
 
 
-// obtain a string 
-var not_rev_word = ""; // <-- variable created to hold given string
-
-//can you point to individual characters in string?
-charAt()
-
-// are there built in Java functions? (like ruby?)
-.split(); // --> separates string characters into individual objects
-.substring() // --> RETURNS new string where charcters are missing, uses index num range as parameter
-"word or string".length // --> gives numeric length of characters (same as ruby)
-.pop()//    removes last element of array
-.push()//   adds element to end of array
-.join()//	Joins all elements of an array into a string.
-
-//how to put data into an array and manipulate order?
-
-//comparisons in java 
-(word === "") // equal value and equal type?
-// can you use IF statments? -->  if (str === "")
-
-
-// source data from array--> convert to string (remember that you can add string in JS?)
-
-//RETURN a reverse string
+//1. count the characters
+//2. use that number -1 to split the word into array
+//3. .pop() method --remove characters from on e array 
+//4. push() to put charcters in new array
+//5. .join method
