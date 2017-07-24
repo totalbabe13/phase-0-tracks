f3ee77f337c36be9e31fe88e24d7c74e1b48fe0c
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-24 00:49:16 -0500

    adding 2 arrays that contain horse names and colors of horses

---
+++ js/data_structures.js
@@ -0,0 +1,13 @@
+
+
+var colors = ["Blue", "brown" ,"white", "flourescent orange"];
+
+
+var horses = ["Ed", "Beauty", "Ghost Rider", "Lady Two-Chains"];
+
+colors.push("Glow in the Dark");
+colors.push("Chrome")
+
+horses.push("Cheeta")
+horses.push("George Washington")
+
