b22a8ee3efbff47cff9bb9e9dd05ff12bcf65345
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-23 21:18:37 -0500

    finding built in functions, pop,push,join

--- js/explore.js
+++ js/explore.js
@@ -8,11 +8,15 @@
 var not_rev_word = ""; // <-- variable created to hold given string
 
 //can you point to individual characters in string?
+charAt()
 
 // are there built in Java functions? (like ruby?)
-string.split([separator][, limit]); // --> separates string characters into individual objects
-(Str.substring() // --> RETURNS new string where charcters are missing, uses index num range as parameter
-str.length // --> gives numeric length of characters (same as ruby)
+.split(); // --> separates string characters into individual objects
+.substring() // --> RETURNS new string where charcters are missing, uses index num range as parameter
+"word or string".length // --> gives numeric length of characters (same as ruby)
+.pop()//    removes last element of array
+.push()//   adds element to end of array
+.join()//	Joins all elements of an array into a string.
 
 //how to put data into an array and manipulate order?
 
