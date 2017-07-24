7ed731196e941250ee336e35ac9cd714db3d7bb0
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-24 16:06:44 -0500

    finished assignment, refactored methods DRY, filled in refletions

--- ruby/my_solutions.rb
+++ ruby/my_solutions.rb
@@ -29,8 +29,8 @@ class VirusPredictor
   #this method calls the other two methods at the same time. it consolidates the other two methods,
   #allowing the us to short hand the other two into one tidy method.
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths #(@population_density, @population, @state)
+    speed_of_spread  #(@population_density, @state)
   end
 
   private
@@ -39,47 +39,33 @@ class VirusPredictor
   # This method compares the population info from "state-data", makes a comparison,
   # and performs a multiplication operation, where the relust is stored in a variable-->
   # "Number of deaths" and the prints a string for the user using interpolation
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+    if @population_density >= 50
+      number_of_deaths = (@population * (@population_density / 50).floor * 0.1).floor
     else
       number_of_deaths = (@population * 0.05).floor
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
   end
 
 
   #This method takes 2 arguments as parameters, the name of the state and the population density.
   #based on the population density data from "State_data" data structure, it creates ranges to multiply it,
   #and stores the result in the "speed" variable given below, to be printed in a string via interpolation.
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
     if @population_density >= 200
       speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
     else
-      speed += 2.5
+      speed += 2.5 - ((@population_density / 50).floor * 0.5)
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
-
   end
 
 end
@@ -114,3 +100,21 @@ alaska.virus_effects
 
 #=======================================================================
 # Reflection Section
+
+#What are the differences between the two different hash syntaxes shown in the state_data file?
+#the state keys are represented each a sindividual strings, where as the population data is 
+#is stored as a symbol (poplation:) the semi-colon notation.
+
+#What does require_relative do? How is it different from require?
+# Required relative, sources data/code from another document, similar to the relationship that css has with HTML
+# You can work with a local source class, or data structure, Where as "require", scopes a sources the Ruby library at large
+
+
+#What are some ways to iterate through a hash?
+#hashes have a many built in iterator methods, such as each, map, ect.. 
+#but also you could use a loop
+
+
+#When refactoring virus_effects, what stood out to you about the variables, if anything?
+#What concept did you most solidify in this challenge? they are technically instance variables, 
+#they are available through-out the class code. 
