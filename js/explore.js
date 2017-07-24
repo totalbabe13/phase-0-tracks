6bd131de1d233b328f79231a3261913b256752d0
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-23 21:06:26 -0500

    finding built in function in JS

--- js/explore.js
+++ js/explore.js
@@ -1,13 +1,25 @@
 
 // 7.1 Leonardo Kaplan
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
 
 // obtain a string 
+var not_rev_word = ""; // <-- variable created to hold given string
 
 //can you point to individual characters in string?
 
 // are there built in Java functions? (like ruby?)
+string.split([separator][, limit]); // --> separates string characters into individual objects
+(Str.substring() // --> RETURNS new string where charcters are missing, uses index num range as parameter
+str.length // --> gives numeric length of characters (same as ruby)
 
 //how to put data into an array and manipulate order?
+
+//comparisons in java 
+(word === "") // equal value and equal type?
+// can you use IF statments? -->  if (str === "")
+
 
 // source data from array--> convert to string (remember that you can add string in JS?)
 
