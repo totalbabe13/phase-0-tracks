6fa638c049aa760a7246431dd31bfac097459822
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-23 22:16:24 -0500

    learning how to use .length inside of split

--- js/explore.js
+++ js/explore.js
@@ -3,11 +3,11 @@
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 function reverser(word){
-return word.length
+ //word.length -1
+ return word.split("",word.length-1)
 }
 
 reverser('apples')
-
 
 //1. count the characters
 //2. use that number -1 to split the word into array
