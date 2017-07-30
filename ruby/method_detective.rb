c0c9f16fc6dd253cc985ea988d6faee9c5058a3b
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-30 18:49:53 -0500

    found first method, used  .swapcase

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,39 +1,40 @@
+
 # Replace in the "<???>" with the appropriate method (and arguments, if any).
 # Uncomment the calls to catch these methods red-handed.
 
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-puts "iNvEsTiGaTiOn".swapcase
+ "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-puts "zom".insert(1,'o')
+# "zom".<???>
 # => “zoom”
 
-puts "enhance".center(15)
+# "enhance".<???>
 # => "    enhance    "
 
-puts "Stop! You’re under arrest!".upcase
+# "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
 
-puts "the usual".concat " suspects"
+# "the usual".<???>
 #=> "the usual suspects"
 
-puts " suspects".prepend "the usual"
+# " suspects".<???>
 # => "the usual suspects"
 
-puts "The case of the disappearing last letter".chop
+# "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
 
-puts  "The mystery of the missing first letter".delete 'T'
+# "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-puts "Elementary,    my   dear        Watson!".squeeze
+# "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
 
-puts "z".ord 
+# "z".<???>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-puts "How many times does the letter 'a' appear in this string?".count 'a'
+# "How many times does the letter 'a' appear in this string?".<???>
 # => 4
