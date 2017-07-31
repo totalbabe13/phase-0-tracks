65a0a951e8703e7ecd68dbd9047467cc664ec30d
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-31 05:51:25 -0500

    creating a hash to practice accessing deeply nested items

---
+++ ruby/new_nested_data_structures.rb
@@ -0,0 +1,20 @@
+
+# Once you've built it, print a few individual pieces of deeply nested 
+# data from the structure, showing that you know how to use multiple 
+# indexes or hash keys (or both) to access nested items. Try to demonstrate 
+# a few different types of access.
+
+highway666 = {
+
+	mazda:['Sally','Greg','Bob','Alma'],
+	
+	toyota:['Jade', 'Lauren','JP']
+	
+	honda: ['Emily','Dana',"Cory"]
+	
+	chevy:['John','Marina','Tony','Rachel','Sofia']
+	
+	ford: ['Ron','Chelsea','Mike','Hope','Ben']
+}
+
+p highway666[mazda:][3]
