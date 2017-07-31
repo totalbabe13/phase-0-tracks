0a6f519448823c95d7f0c6c96241d46854287255
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-30 19:15:35 -0500

    found eleventh method, .count

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -36,5 +36,5 @@ puts "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
