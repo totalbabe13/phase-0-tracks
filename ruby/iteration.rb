8666e33810490c6b46a8a1ad6f384b4f573fde3a
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-31 03:56:08 -0500

    completed part 2 of release 2 in 5.3

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -67,12 +67,6 @@ spacer
 
 #  = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
-#1. A method that iterates through the items, deleting any that
-#   meet a certain condition (for example, deleting any numbers that are less than 5).
-
-#2. A method that filters a data structure for only items that 
-#   do satisfy a certain condition (for example, keeping any numbers that are less than 5).
-
 #3. A different method that filters a data structure for only items 
 #   satisfying a certain condition -- Ruby offers several options!
 
@@ -80,11 +74,24 @@ spacer
 #   condition in the block evaluates to false, then stops (you may 
 #   not find a perfectly working option for the hash, and that's okay).
 
+#  = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
 example_array = [1,3,7,0,6,1,44,28,93,31,77,62]
-
+#1. A method that iterates through the items, deleting any that
+#   meet a certain condition (for example, deleting any numbers that are less than 5).
 example_array.delete_if {|number| number.even? }
-
 p example_array
+
+#  = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+spacer
+array_ray_ray = [0,0,0,1,0,9,10,88,7,6,66,4,2,3,5]
+#2. A method that filters a data structure for only items that 
+#   do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+array_ray_ray.keep_if  { |number| number < 5 }
+p array_ray_ray
+
+
 
 
 
