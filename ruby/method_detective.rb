a63bc59614afc01b1cd085842d6846415f43c6cc
totalbabe13 <kaplanleonardo@gmail.com>
2017-07-30 19:14:44 -0500

    found tenth method, .ord

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -32,7 +32,7 @@ puts "The mystery of the missing first letter".delete "T"
 puts "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
