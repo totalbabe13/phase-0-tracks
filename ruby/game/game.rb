c84d0400cff19dd2f8b7fb41931a12f4c80c37ce
totalbabe13 <kaplanleonardo@gmail.com>
2017-08-02 03:03:07 -0500

    created game file

---
+++ ruby/game/game.rb
@@ -0,0 +1,49 @@
+#Using plain old English in a text file or other scratch pad, 
+#pseudocode a class for a word-guessing game that meets the following specifications. 
+#Assume your driver code will handle input and output as far as the user is concerned. 
+#In other words, write a class designed for a computer program to use, not one designed 
+#for a human to use directly, then use driver code to bridge the gap between human and Ruby object.
+
+#One user can enter a word (or phrase, if you would like your game to support that), 
+#and another user attempts to guess the word.
+#Guesses are limited, and the number of guesses available is related to the length of the word.
+#Repeated guesses do not count against the user.
+#The guessing player receives continual feedback on the current state of the word. 
+#So if the secret word is "unicorn", the user will start out seeing something like "_ _ _ _ _ _ _", 
+#which would become "_ _ _ c _ _ _" after the user enters a guess of "c".
+#The user should get a congratulatory message if they win, and a taunting message if they lose.
+
+
+class Game
+
+attr_reader
+attr_accessor
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+def initialize
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end	#end of class
+# - - - - - - - - - - - - - - - - - - - - - - - - - 
+#driver code
+
+#new_game = Game.new 
+puts "welome"
+puts "submitt your word player one:"
+
+player1_word = gets.chompS
+
