dd74e9806b186ec8564ee598eb99b25fa429e422
totalbabe13 <kaplanleonardo@gmail.com>
2017-08-14 00:57:45 -0500

    refactored virus effects method, to be DRY

--- ruby/gps6/my_solutions.rb
+++ ruby/gps6/my_solutions.rb
@@ -14,55 +14,56 @@ class VirusPredictor
     @state = state_of_origin
     @population = population
     @population_density = population_density
+    @number_of_deaths = 0
+    @speed = 0
   end
 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
-  end
+    predicted_deaths#(@population_density, @population, @state)
+    speed_of_spread#(@population_density, @state)
+    
+    print "#{@predicted_deaths_output}#{@speed_output}  "
 
-  def full_report(state_data)
-  print STATE_DATA
-  end  
+  end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths#(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      @number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      @number_of_deaths = (@population * 0.3).floor
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      @number_of_deaths = (@population * 0.2).floor
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      @number_of_deaths = (@population * 0.1).floor
     else
-      number_of_deaths = (@population * 0.05).floor
+      @number_of_deaths = (@population * 0.05).floor
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    @predicted_deaths_output = "#{@state} will lose #{@number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
+    @speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      @speed += 0.5
     elsif @population_density >= 150
-      speed += 1
+      @speed += 1
     elsif @population_density >= 100
-      speed += 1.5
+      @speed += 1.5
     elsif @population_density >= 50
-      speed += 2
+      @speed += 2
     else
-      speed += 2.5
+      @speed += 2.5
     end
 
-    puts " and will spread across the state in #{speed} months.\n\n"
+    @speed_output = " and will spread across the state in #{@speed} months.\n\n"
 
   end
 
@@ -73,21 +74,24 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-# Right now this is only running a report for 4 states, and it's not incredibly DRY.
-# Create a report for all 50 states in a DRY way. 
-# Does this belong inside the class or outside of it? Why?
+
 puts
 puts
 puts 'x--x--x--x--x'
-
-
 STATE_DATA.each do |state_name, data|
-
 state = VirusPredictor.new(state_name, STATE_DATA["#{state_name}"][:population_density], STATE_DATA["#{state_name}"][:population])
 state.virus_effects
 end
-
 puts 'x--x--x--x--x'
+
+
+# Right now this is only running a report for 4 states, and it's not incredibly DRY.
+# Create a report for all 50 states in a DRY way. 
+# Does this belong inside the class or outside of it? Why?
+
+
+ 
+
 
 #alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 #alabama.virus_effects
@@ -101,7 +105,7 @@ puts 'x--x--x--x--x'
 #alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 #alaska.virus_effects
 
-puts 'x--x--x--x--x'
+
 
 
 #=======================================================================
