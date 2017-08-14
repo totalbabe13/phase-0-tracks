81b5bef67809377ba3271f456bfdd7538791f928
totalbabe13 <kaplanleonardo@gmail.com>
2017-08-14 04:14:47 -0500

    finshed module 7-1

--- js/explore.js
+++ js/explore.js
@@ -6,13 +6,13 @@ function reverser(word){
  
 
  last =  word.length 
- 
+ //1. count the characters
  
  array_1 = word.split("",word.length)
-
+//2. use that number -1 to split the word into array
  
  array_2 = array_1.reverse();
-
+//3. .pop() method --remove characters from on e array
 
 
  console.log(array_2.join(''))
@@ -20,11 +20,6 @@ function reverser(word){
  //console.log(array_1)
  //console.log (word.split("",word.length))
 }
-
+//4. use .join function
 reverser('stressed')
 
-//1. count the characters
-//2. use that number -1 to split the word into array
-//3. .pop() method --remove characters from on e array 
-//4. push() to put charcters in new array
-//5. .join method
